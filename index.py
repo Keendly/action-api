409dd3b4b19125bff5646feac3e7f384df930ba5
MoOmEeN <moomeen@gmail.com>
2016-11-07 01:43:08 +0100

    return result

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ import reader_api.reader_api
 
 def handle(event, context):
     """Lambda function entry point """
-    reader_api.reader_api.handle(event)
+    return reader_api.reader_api.handle(event)
