dfd01a3cb5cfc21cbc8a18512c3bfbed0698eca3
MoOmEeN <moomeen@gmail.com>
2016-11-14 01:56:35 +0100

    add sleep

--- reader_api/reader_api.py
+++ reader_api/reader_api.py
@@ -3,12 +3,15 @@
 
 from handler import execute_action, generate_links
 
+import time
+
 def handle(event):
     if is_http(event):
         # execute API action
         return execute_action.handle(event)
     else:
         # generate API action links
+        time.sleep(10)
         return generate_links.handle(event)
 
 
