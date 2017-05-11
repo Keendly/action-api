53ada17e5baf117620f31db938c125870ea769cd
MoOmEeN <moomeen@gmail.com>
2017-05-12 01:06:37 +0200

    change action strings

--- reader_api/handler/constants.py
+++ reader_api/handler/constants.py
@@ -5,9 +5,9 @@ SAVE_ARTICLE = 's'
 MARK_AS_READ_TEXT = 'Click to mark as read'
 MARK_AS_UNREAD_TEXT = 'Click to keep unread'
 SAVE_TEXT = {
-    'default': 'Click to save article',
-    'inoreader': 'Click to star article',
-    'oldreader': 'Click to star article'
+    'default': 'Save article',
+    'inoreader': 'Star article',
+    'oldreader': 'Star article'
 }
 
 # action fields
